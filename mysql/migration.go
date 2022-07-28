78eaa2d6c0c8077843f2162a34775115da6e38a5
Daniel Peach <daniel.peach@armory.io>
2022-07-28 12:20:25 -0400

    fix(migration): include required imports for migrations (#7)

--- mysql/migration.go
+++ mysql/migration.go
@@ -21,6 +21,8 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	log "github.com/sirupsen/logrus"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
