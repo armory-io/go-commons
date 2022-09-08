c371bd1d48c4636fd3c8c4c9b7ceb3bebb29bcec
Dan Peach <daniel.peach@armory.io>
2022-09-08 09:56:52 -0400

    feat(rbac): add scopes package CDAAS-1573

---
+++ iam/scopes/scopes.go
@@ -0,0 +1,148 @@
+package scopes
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type (
+	Type       string
+	Resource   string
+	Permission string
+)
+
+const (
+	ScopeDelimiter = ":"
+
+	TypeAPI         Type = "api"
+	TypeTargetGroup Type = "targetGroup"
+	TypeAccount     Type = "account"
+
+	ResourceDeployments  Resource = "deployments"
+	ResourceTenant       Resource = "tenant"
+	ResourceOrganization Resource = "organization"
+	ResourceAgentHub     Resource = "agentHub"
+	ResourceStar         Resource = "*"
+
+	PermissionFull Permission = "full"
+)
+
+var (
+	types       = []Type{TypeAPI, TypeAccount, TypeTargetGroup}
+	permissions = []Permission{PermissionFull}
+)
+
+var (
+	ErrInvalidScope = errors.New("invalid scope")
+	ErrInvalidGrant = errors.New("invalid grant")
+)
+
+type Grant struct {
+	Type       Type
+	Resource   Resource
+	Permission Permission
+}
+
+func GrantFromStrings(t, r, p string) Grant {
+	return Grant{Type: Type(t), Resource: Resource(r), Permission: Permission(p)}
+}
+
+func Parse(scope string) (Grant, error) {
+	parts := strings.Split(scope, ScopeDelimiter)
+	if len(parts) != 3 {
+		return Grant{}, fmt.Errorf("%w: wanted 3 parts, got %d", ErrInvalidScope, len(parts))
+	}
+
+	t := Type(parts[0])
+	r := Resource(parts[1])
+	p := Permission(parts[2])
+
+	v := validator{baseError: ErrInvalidScope}
+	if err := v.validate(t, r, p); err != nil {
+		return Grant{}, err
+	}
+
+	return Grant{
+		Type:       t,
+		Resource:   r,
+		Permission: p,
+	}, nil
+}
+
+func FromGrant(g Grant) (string, error) {
+	t := g.Type
+	r := g.Resource
+	p := g.Permission
+
+	v := validator{baseError: ErrInvalidGrant}
+
+	if err := v.validate(t, r, p); err != nil {
+		return "", err
+	}
+
+	return string(t) + ScopeDelimiter + string(r) + ScopeDelimiter + string(p), nil
+}
+
+type validator struct {
+	baseError error
+}
+
+func (v validator) validate(t Type, r Resource, p Permission) error {
+	if err := v.validateType(t); err != nil {
+		return err
+	}
+
+	if err := v.validatePermission(p); err != nil {
+		return err
+	}
+
+	switch t {
+	case TypeAPI:
+		if err := v.validateResourceForAPIType(r); err != nil {
+			return err
+		}
+	case TypeTargetGroup:
+		if err := v.validateResourceForTargetGroupType(r); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (v validator) validateType(t Type) error {
+	if !oneOf(types, t) {
+		return fmt.Errorf("%w: unexpected type %q", v.baseError, t)
+	}
+	return nil
+}
+
+func (v validator) validatePermission(p Permission) error {
+	if !oneOf(permissions, p) {
+		return fmt.Errorf("%w: unexpected permission %q", v.baseError, p)
+	}
+	return nil
+}
+
+func (v validator) validateResourceForAPIType(r Resource) error {
+	if !oneOf([]Resource{ResourceDeployments, ResourceTenant, ResourceOrganization, ResourceAgentHub}, r) {
+		return fmt.Errorf("%w: invalid resource for api type: %q", v.baseError, r)
+	}
+	return nil
+}
+
+func (v validator) validateResourceForTargetGroupType(r Resource) error {
+	if r == ResourceStar {
+		return fmt.Errorf("%w: cannot use wildcard resource for targetGroup type", v.baseError)
+	}
+	return nil
+}
+
+func oneOf[T comparable](group []T, test T) bool {
+	for _, g := range group {
+		if test == g {
+			return true
+		}
+	}
+	return false
+}
---
+++ iam/scopes/scopes_test.go
@@ -0,0 +1,97 @@
+package scopes
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	valid := []pair[string, Grant]{
+		pairFrom("api:organization:full", GrantFromStrings("api", "organization", "full")),
+		pairFrom("api:tenant:full", GrantFromStrings("api", "tenant", "full")),
+		pairFrom("api:deployments:full", GrantFromStrings("api", "deployments", "full")),
+		pairFrom("api:agentHub:full", GrantFromStrings("api", "agentHub", "full")),
+		pairFrom("targetGroup:potato-facts:full", GrantFromStrings("targetGroup", "potato-facts", "full")),
+		pairFrom("account:eks-dev-cluster:full", GrantFromStrings("account", "eks-dev-cluster", "full")),
+		pairFrom("account:*:full", GrantFromStrings("account", "*", "full")),
+	}
+
+	for _, v := range valid {
+		t.Run(fmt.Sprintf("valid: %s", v.left), func(t *testing.T) {
+			grant, err := Parse(v.left)
+			assert.NoError(t, err)
+			assert.Equal(t, v.right, grant)
+		})
+	}
+
+	invalid := []string{
+		"dachshund:deployments:full",
+		"api:chihuahua:full",
+		"api:deployments:pomeranian",
+		"too:many:parts:here",
+		"targetGroup:*:full",
+		"api:*:full",
+		"::",
+		"api::full",
+		":*:full",
+		":*:",
+	}
+
+	for _, i := range invalid {
+		t.Run(fmt.Sprintf("invalid: %s", i), func(t *testing.T) {
+			_, err := Parse(i)
+			assert.Error(t, err)
+			assert.ErrorIs(t, err, ErrInvalidScope)
+		})
+	}
+}
+
+func TestGrantFrom(t *testing.T) {
+	valid := []pair[Grant, string]{
+		pairFrom(GrantFromStrings("api", "organization", "full"), "api:organization:full"),
+		pairFrom(GrantFromStrings("api", "tenant", "full"), "api:tenant:full"),
+		pairFrom(GrantFromStrings("api", "deployments", "full"), "api:deployments:full"),
+		pairFrom(GrantFromStrings("api", "agentHub", "full"), "api:agentHub:full"),
+		pairFrom(GrantFromStrings("targetGroup", "potato-facts", "full"), "targetGroup:potato-facts:full"),
+		pairFrom(GrantFromStrings("account", "eks-dev-cluster", "full"), "account:eks-dev-cluster:full"),
+		pairFrom(GrantFromStrings("account", "*", "full"), "account:*:full"),
+	}
+
+	for _, v := range valid {
+		t.Run(fmt.Sprintf("valid: %s", v.left), func(t *testing.T) {
+			scope, err := FromGrant(v.left)
+			assert.NoError(t, err)
+			assert.Equal(t, v.right, scope)
+		})
+	}
+
+	invalid := []Grant{
+		GrantFromStrings("dachshund", "deployments", "full"),
+		GrantFromStrings("api", "chihuahua", "full"),
+		GrantFromStrings("api", "deployments", "pomeranian"),
+		GrantFromStrings("targetGroup", "*", "full"),
+		GrantFromStrings("api", "*", "full"),
+		GrantFromStrings("", "", ""),
+		GrantFromStrings("api", "", "full"),
+		GrantFromStrings("", "*", "full"),
+		GrantFromStrings("", "*", ""),
+	}
+
+	for _, i := range invalid {
+		t.Run(fmt.Sprintf("invalid: %s", i), func(t *testing.T) {
+			_, err := FromGrant(i)
+			assert.Error(t, err)
+			assert.ErrorIs(t, err, ErrInvalidGrant)
+		})
+	}
+}
+
+type pair[T, U any] struct {
+	left  T
+	right U
+}
+
+func pairFrom[T, U any](left T, right U) pair[T, U] {
+	return pair[T, U]{left, right}
+}
